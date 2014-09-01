7021405c21f6ef0d2ee4d92c1b7a92756af96082
cshuig@126.com <a8694420>
2014-09-01 20:12:25 +0800

    删除

---
+++ DBToBean/src/main/java/DBcommon/DBConnection.java
@@ -0,0 +1,126 @@
+package DBcommon;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ * 数据库连接初始化类
+ * 数据库种类： mySql、oracle、DB2
+ * 通过构造函数来初始化不同的数据库
+ * Created by Administrator on 2014/8/29 0029.
+ */
+public class DBConnection {
+
+    /** 数据库类型 **/
+    private static final String DB_ORACLE = "oracle";
+    private static final String DB_MYSQL = "mysql";
+    private static final String DB_DB2 = "db2";
+
+    /**
+     * 数据库：地址
+     */
+    private String dbUrl;
+    /**
+     * 数据库：驱动名
+     * 不同的数据库，驱动都不同
+     * mysql：   com.mysql.jdbc.Driver
+     * oracle：  oracle.jdbc.driver.OracleDriver
+     */
+    private String dbDriver;
+    /**
+     * 数据库：类型
+     * 如上三种
+     */
+    private String dbType;
+    /**
+     * 数据库：名字
+     */
+    private String dbName;
+    /**
+     * 数据库：登陆用户名
+     */
+    private String username;
+    /**
+     * 数据库：登陆密码
+     */
+    private String password;
+
+    public DBConnection() {
+    }
+
+    /**
+     * 通过构造函数来初始化数据库连接的信息
+     * @param dbUrl         数据库地址
+     * @param dbType        数据库类型
+     * @param dbName        数据库名字
+     * @param username      数据库登陆用户名
+     * @param password      数据库登入密码
+     */
+    public DBConnection(String dbUrl, String dbType, String dbName, String username, String password) {
+        this.dbUrl = dbUrl;
+        switch (dbType){
+            case DB_MYSQL :
+                dbDriver = "com.mysql.jdbc.Driver";
+                break;
+            case DB_ORACLE :
+                dbDriver = "oracle.jdbc.driver.OracleDriver";
+                break;
+        }
+        this.dbName = dbName;
+        this.username = username;
+        this.password = password;
+    }
+
+    /**
+     * 获取数据库的连接对象
+     * @return  返回一个数据库连接 (Connection) 对象
+     * @throws ClassNotFoundException
+     * @throws java.sql.SQLException
+     */
+    public Connection getConnection() {
+        Connection connection;
+        try {
+            //通过Class.forName,来创建数据库驱动类对象,同时自动注册 DriverMananger,参数必须是带全路径的包名
+            Class.forName(dbDriver);
+            connection = (Connection) DriverManager.getConnection(dbUrl,username,password);
+            return connection;
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    /**
+     *关闭数据库连接
+     * @param connection
+     */
+    public void closeConnection(Connection connection){
+        try {
+            if(connection != null && !connection.isClosed()) {
+                connection.close();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * 判断字符串是否为空
+     * @param str
+     * @return
+     */
+    public boolean checkIsNull(String str){
+        boolean flag = false;
+        if(str==null || "".equals(str)){
+            flag = true;
+        }
+        return flag;
+    }
+
+    public static void main(String[] args){
+        new DBConnection("jdbc:mysql://localhost:3306/",DB_MYSQL,"hibernate","root","").getConnection();
+    }
+}
---
+++ DBToBean/src/main/java/DBcommon/DBFactory.java
@@ -0,0 +1,62 @@
+package DBcommon;
+
+import java.sql.*;
+import java.util.Vector;
+
+/**
+ * 数据库工厂类
+ * Created by Administrator on 2014/8/29 0029.
+ */
+public class DBFactory {
+
+    /** 数据库连接的对象 */
+    private static DBConnection connection = null;
+
+    /**
+     * 获取数据库连接的对象
+     * @param dbUrl         数据库连接
+     * @param dbType        数据库类型
+     * @param dbName        数据库名字
+     * @param username      数据库登陆用户名
+     * @param password      数据库登陆密码
+     * @return
+     */
+    public static DBConnection newDBConnectionInstance(String dbUrl,String dbType,String dbName,String username,String password){
+        connection = new DBConnection(dbUrl,dbType,dbName,username,password);
+        return connection;
+    }
+
+    /**
+     * 关闭数据库连接对象
+     * @param _connection   数据库连接对象
+     */
+    public static void closeConnection(Connection _connection){
+        connection.closeConnection(_connection);
+    }
+
+    /**
+     * 获取指定表的所有列名
+     * @param conn          数据库连接对象
+     * @param tableName     指定的：表名
+     * @return  所有列名
+     */
+    public static Vector getColumns(Connection conn,String tableName){
+        Vector vector = new Vector();
+        //获取预编译SQL的对象
+        try {
+            PreparedStatement preparedStatement = conn.prepareStatement(" SELECT * FROM " +tableName);
+            ResultSet  resultSet = preparedStatement.executeQuery();
+            ResultSetMetaData resultSetMetaData = resultSet.getMetaData();
+            int count = resultSetMetaData.getColumnCount();
+            //注意，是从1开始
+            for(int i=1;i<=count;i++){
+                System.out.println(tableName +">> 字段"+i+"的名字：" + resultSetMetaData.getColumnName(i));
+                vector.add(resultSetMetaData.getColumnName(i));
+            }
+            return vector;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
