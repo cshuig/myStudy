851dc3bd96d2b297adafb185d8be8ea5b4d0f338
cshuig@126.com <a8694420>
2014-09-01 21:48:57 +0800

    删除多余的import

--- Hibernate/Hibernate_Annotation/src/main/java/com/cshuig/entity/Author.java
+++ Hibernate/Hibernate_Annotation/src/main/java/com/cshuig/entity/Author.java
@@ -3,14 +3,10 @@ package com.cshuig.entity;
 import java.util.HashSet;
 import java.util.Set;
 
-import javax.annotation.Generated;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.JoinColumns;
-import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 
