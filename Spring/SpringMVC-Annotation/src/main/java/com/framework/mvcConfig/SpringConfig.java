b6f09d18d3769fda77e19aa523923638f448f214
cshuig@126.com <a8694420>
2014-09-28 19:16:26 +0800

    ssssssss

---
+++ Spring/SpringMVC-Annotation/src/main/java/com/framework/mvcConfig/SpringConfig.java
@@ -0,0 +1,22 @@
+package com.framework.mvcConfig;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.stereotype.Controller;
+
+/**
+ * 此类等价于 spring-config.xml
+ * Created by Administrator on 2014/9/15.
+ * 备注：
+ *  1、ComponentScan注解中需要排除：Controller注解的扫描，因为springmvc，默认已经扫描了，会冲突
+ */
+@Configuration
+@ComponentScan(basePackages = "com.cshuig",
+        excludeFilters = { @ComponentScan.Filter(type = FilterType.ANNOTATION,
+                value = {Controller.class}
+        )
+        }
+)
+public class SpringConfig {
+}
---
+++ Spring/SpringMVC-Annotation/src/main/java/com/utils/AppVersionTest.java
@@ -0,0 +1,46 @@
+package com.utils;
+
+import org.junit.Ignore;
+import org.junit.Test;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ * 版本号管理测试类
+ * @author Administrator
+ */
+@Transactional(propagation = Propagation.REQUIRED)
+public class AppVersionTest extends BaseJunit{
+
+
+    /**
+     * 测试get请求
+     */
+    @Test
+    @Ignore
+    public void testGet(){
+        try {
+            mockMvcGet("/admin/AppVersionGridModel");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    /**
+     * 测试post请求
+     * 请求的参数格式：json
+     */
+    @Test
+    @Ignore
+    public void testPost(){
+        try {
+        	//String json =  "{\"announcement_id\": 110,\"user_id\": 10003,\"content\": \"请输入内容\"}";
+
+            String json = "{\"versionCode\":\"1.0.3\",\"updateType\":\"choose\",\"status\":\"wfb\",\"description\":\"更新详细\",\"created\":1410939046000,\"appType\":\"ios\"}";
+            mockMvcPost("/admin/saveOrUpdateAppversion", json);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
